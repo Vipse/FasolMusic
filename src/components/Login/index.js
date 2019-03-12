605bdebae95900d6c852a6cdae651186582ee9f1
dmi3q <kunakov.dmitry@gmail.com>
2019-03-12 17:33:08 +0300

    More design improvements to loginPage

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -21,7 +21,7 @@ const FormItem = Form.Item;
 class LoginForm extends React.Component{
     state = {
         showSocialLogin: false,
-        activePage: 'signin',
+        activePage: 'signup',
         avatar: '',
         facebookLink: '',
         googleLink: '',
