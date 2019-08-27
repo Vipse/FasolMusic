2b0ddcf04180dedcc4ce3b1f8262fae14259911f
latiertiw <latierti@yandex.ru>
2019-08-27 14:02:58 +0300

    remove bad var + fix 'isAfter'

--- src/components/NearTrainingsItem/index.js
+++ src/components/NearTrainingsItem/index.js
@@ -33,7 +33,7 @@ class NearTrainingsItem extends React.Component{
             user_mode
         } = this.props;
         
-        const isAfter = moment(+start).subtract(2, 'hour') > moment() ? false : true
+        const isAfter = moment(+start) > moment() ? false : true
 
         
         return (
@@ -74,7 +74,6 @@ class NearTrainingsItem extends React.Component{
                     {this.props.isTrial && !this.state.deleteTrialDisabled ? 
                         <div className="btn-push">
                             <Button 
-                                disable={moment() < beginTime ? true:false}
                                 btnText="Не пришел"
                                 type="border-green"
                                 size='small'
